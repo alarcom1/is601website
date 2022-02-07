6ae74c5110f2165300c9b0d9dc10dc03d4fd5d71
mauricio alarco <ma2546@njit.edu>
2022-02-06 21:20:34 -0500

    changed the name in the controller index html

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Mauricio AlarcoHildebrandt"
         return render_template('index.html', name=name)
